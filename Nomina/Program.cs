194f71f9eaa44be6ce9175cc4716360d4b34c1df
Jstacy12 <38300105+Jstacy12@users.noreply.github.com>
2018-04-17 23:38:18 -0400

    Pimera

---
+++ Nomina/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nomina
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Johan De Los santos - 10-SISN-1-011 - 0908
+            // Arthur Henriquez - 12-EISN-1-214 - 0908
+
+            string nombre;
+            int salario, horasTrabajadas, SalarioHora, SalarioMinimo;
+
+            Console.WriteLine("Ingrese su nombre");
+             nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese cuantas horas trabajó: ");
+            horasTrabajadas = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese el valor de las horas: ");
+            SalarioHora = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese el salario minimo: ");
+            SalarioMinimo = Convert.ToInt32(Console.ReadLine());
+
+            string estudiante1 = "Johann De Los Santos";
+            string matricula1 = "10-SISN-1-011";
+
+            string estudiante2 = "Arthur Henriquez";
+            string matricula2 = "12-EISN-1-214";
+
+
+            salario = horasTrabajadas * SalarioHora;
+
+            if(salario > SalarioMinimo)
+            {
+                Console.WriteLine("Nombre: " + nombre);
+                Console.WriteLine("Salario menusal: " + salario + "\n");
+            }
+
+            if( salario < SalarioMinimo)
+            {
+                Console.WriteLine("Nombre: " + nombre + "\n");
+            }
+
+            if(salario == SalarioMinimo)
+            {
+                Console.WriteLine("Nombre: " + nombre + "\n");
+                Console.WriteLine("Salario: " + salario + "que es igual al valor del salario minimo: " + SalarioMinimo + "\n");
+            }
+
+            Console.WriteLine(estudiante1 + "" + matricula1 );
+            Console.WriteLine(estudiante2 + "" + matricula2 );
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Numero positivos/Numero positivos/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numero_positivos
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            // Johan De Los santos - 10-SISN-1-011 - 0908
+            // Arthur Henriquez - 12-EISN-1-214 - 0908
+
+
+            int n, valor, positivos, negativos, pares, impares, ciclo;
+            positivos = 0;
+            negativos = 0;
+            pares = 0;
+            impares = 0;
+            ciclo = 0;
+            string linea;
+            Console.Write("Ingrese valor de n: ");
+            linea = Console.ReadLine();
+            n = int.Parse(linea);
+
+            string estudiante1 = "Johann De Los Santos";
+            string matricula1 = "10-SISN-1-011";
+
+            string estudiante2 = "Arthur Henriquez";
+            string matricula2 = "12-EISN-1-214";
+
+
+            try
+            {
+                    for (int i = 1; i <= n; i++)
+                    {
+                        Console.Write("Ingrese numero " + i + ": ");
+                        linea = Console.ReadLine();
+                        valor = int.Parse(linea);
+                        if (valor > 0)
+                        {
+                            positivos++;
+                        }
+                        else
+                        {
+                            if (valor < 0)
+                            {
+                                negativos++;
+                            }
+                        }
+                        if (valor % 2 == 0)
+                        {
+                            pares++;
+                        }
+                        else
+                        {
+                            if (valor % 2 != 0)
+                            {
+                                impares++;
+                            }
+                        }
+                    }
+
+                    for( int k = 0; k <= n; k++)
+                {
+                    ciclo = k++;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("Numero invalido" + ex);
+            }
+
+            Console.WriteLine("Cantidad de numeros negativos: " + negativos);
+            Console.WriteLine("Cantidad de numeros positivos: " + positivos);
+            Console.WriteLine("Cantidad de numeros pares: " + pares);
+            Console.WriteLine("Cantidad de numeros impares: " + impares);
+            Console.WriteLine("Las veces que se ejecutó el ciclo for fueron: " + ciclo + "\n");
+
+            Console.WriteLine(estudiante1 + " " + matricula1);
+            Console.WriteLine(estudiante2 + " " + matricula2);
+            Console.ReadKey();
+        }
+    }
+}
+        
